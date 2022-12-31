3ff0799dc68f48a4de657d9b3ca7bc44f3e3c678
Chris Nickell <chrisjnickell@gmail.com>
2022-12-31 11:27:33 -0700

    Hmmm not sure

---
+++ day07/day7.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+import numpy as np
+
+with open('input.txt') as fh:
+    data = [line.strip() for line in fh.readlines()]
+
+vars = defaultdict(np.uint16)
+
+for line in data:
+    sp = line.split()
+    if 'AND' in line:
+        try:
+            left = np.uint16(sp[0])
+        except:
+            left = vars[sp[0]]
+        right = vars[sp[2]]
+        dest = sp[-1]
+
+        vars[dest] = np.uint16(left & right)
+    elif 'OR' in line:
+        try:
+            left = np.uint16(sp[0])
+        except:
+            left = vars[sp[0]]
+        right = vars[sp[2]]
+        dest = sp[-1]
+
+        vars[dest] = np.uint16(left | right)
+    elif 'LSHIFT' in line:
+        vars[sp[-1]] = np.uint16(vars[sp[0]] << int(sp[2]))
+    elif 'RSHIFT' in line:
+        vars[sp[-1]] = np.uint16(vars[sp[0]] >> int(sp[2]))
+    elif 'NOT' in line:
+        vars[sp[-1]] = np.uint16(~vars[sp[1]])
+    else:
+        try:
+            vars[sp[-1]] = np.uint16(sp[0])
+        except ValueError:
+            vars[sp[-1]] = vars[sp[0]]
+
+print(vars['a'])
